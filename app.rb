d0174fb1a4f5b01a9c1f5d5b0633ac01686a990d
Daniel <danieleduardocald@gmail.com>
2021-01-11 14:09:00 -0500

    commit tree 010a84834cb8ff3af20d19418d3b42cc228dbac3

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Shotgun is the bestt!!!!!!!"
+    
+
   end
 
 end
